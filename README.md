ce70fa8dd9548d1cf457ad968eecf7852b783f24
sjproctor <sproctor950@gmail.com>
2020-07-30 14:21:03 -0700

    edits for clarity

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Rails Active Record Query Syntax
 
 ## Overview
-- ActiveRecord is a crown jewel of the Rails framework.
+- Active Record is a crown jewel of the Rails framework.
 - Rails and Active Record have an incredibly rich query syntax to help your code that looks up information in the database readable and maintainable.
 
 ## Learning Objectives
 - Interacting with Active Record query syntax
-- Exploring the differences between SQL queries and Active Record
+- Exploring the differences and similarities between SQL queries and Active Record
 
 ### Setup:
 - git clone
@@ -25,38 +25,38 @@
 - $ rspec spec/models/country_query_specs.rb
 
 
+## Active Record Query Syntax
+Active Record query syntax is a mesh between Ruby code and SQL clauses. It provides the developer a lot of power to make very specific database queries.
+
 ### WHERE and FIRST
-Find the first country in the database with a code of 'RUS' :
+Find the first country in the database with a code of 'RUS':
 ```ruby
 russia = Country.where(code: 'RUS').first
-# Generates the following sql:
-# "SELECT \"country\".* FROM \"country\" WHERE \"country\".\"code\" = 'RUS'"
+# Generates the following SQL:
+# "SELECT "country".* FROM "country" WHERE "country"."code" = 'RUS'"
 ```
-notice the `.first` at the end of that query.  the code `Country.where(code: 'RUS')` returns an Active Record Relation, which is a lot like an array. We only are interested in the first result, so we call `.first` and get back the first instance in that arrayish thing.
+Notice the `.first` at the end of that query. The code `Country.where(code: 'RUS')` returns an Active Record Relation, which is a lot like an array. We only are interested in the first result, so we call `.first` and get back the first instance in that array-ish thing.
 
 ### Multiple WHERE Clauses
 We can chain together multiple clauses in our search:
 ```ruby
-countries = Country
-  .where(continent: "Asia")
-  .where("surfacearea > 100000")
-# Generates sql:
-"SELECT \"country\".* FROM \"country\" WHERE \"country\".\"continent\" = 'Asia' AND (surfacearea > 100000)"
+countries = Country.where(continent: "Asia").where("surfacearea > 100000")
+# Generates SQL:
+# "SELECT "country".* FROM "country" WHERE "country"."continent" = 'Asia' AND (surfacearea > 100000)"
 ```
 
 This time we get a set of countries back of course, because there is more than one. Cool!
 
 ### Custom Selects
-We can use custom selects too, if we need to do something unique.  Checkout this query:
+We can use custom selects too, if we need to do something unique. Checkout this query:
 ```ruby
-countries = Country
-  .where("governmentform LIKE '%Monarchy%'")
+countries = Country.where("governmentform LIKE '%Monarchy%'")
 
-# Generates sql:
-"SELECT \"country\".* FROM \"country\" WHERE (governmentform LIKE '%Monarchy%')"
+# Generates SQL:
+# "SELECT "country".* FROM "country" WHERE (governmentform LIKE '%Monarchy%')"
 ```
 
-That opens up a whole new realm of possibilities, doesn't it? If you can dream up a query you want to run, you can probably do it with Active Record's flexible where method.
+That opens up a whole new realm of possibilities, doesn't it? If you can dream up a query you want to run, you can probably do it with Active Record's flexible `.where` method.
 
 ### ORDER and LIMIT
 
@@ -68,14 +68,13 @@ country = Country
   .limit(1)
   .first
 
-# generates the sql:
-# "SELECT \"country\".* FROM \"country\" ORDER BY surfacearea ASC LIMIT 1"
+# generates the SQL:
+# "SELECT "country".* FROM "country" ORDER BY surfacearea ASC LIMIT 1"
 
 # note: "surfacearea DESC" would reverse that sort order
 ```
 
 ### PLUCK
-
 Pluck is a cool tool to get just one attribute of a record returned to you in an array.  We use it like this:
 
 ```ruby
@@ -84,7 +83,7 @@ countries = Country
   .limit(10)
   .pluck(:name)
 
-# Generates the sql:
+# Generates the SQL:
 # SELECT "country"."name" FROM "country" ORDER BY "country"."surfacearea" ASC LIMIT 10
 
 # and returns
@@ -92,7 +91,6 @@ countries = Country
 ```
 
 ### Aggregates Like SUM
-
 If you need to sum up a column in your query, you can do that as well.  What if we wanted to sum up all the land mass of Europe?  
 
 ```ruby
--- spec/models/country_query_specs.rb
+++ spec/models/country_query_specs.rb
@@ -1,79 +1,82 @@
 require 'rails_helper'
 
-RSpec.describe "Practice with ActiveRecord", type: :model do
-  describe "Where" do
+RSpec.describe Country, type: :model do
+  describe "practice with Active Record using WHERE syntax" do
     it "can find records and attributes (class)" do
-      #What is the population of the US?
+      # What is the population of the US?
       us = Country.where(code: 'USA').first
+      # This test passes!
       expect(us.population).to eq(278357000)
     end
 
     it "can find records and attributes" do
-      #What is the area of the US?
-      us = Country.where(code: 'USA').first
+      # What is the area of the US?
       expect(us.surfacearea).to eq(9363520.0)
     end
 
     it "can find records and attributes" do
-      #What is the population of Canada?
+      # What is the population of Canada?
       canada = Country.where(code: 'CAN').first
+      # This test passes!
       expect(canada.population).to eq(31147000)
     end
 
     it "can find records and attributes" do
-      #What is the area of Canada?
+      # What is the area of Canada?
       expect(canada.surfacearea).to eq(9970610.0)
     end
 
-
     it "can find records via equality comparrison (class)" do
-      #List the number of countries in Europe that have a surface area greater than 200,000 km squared.
+      # List the number of countries in Europe that have a surface area greater than 200,000 km squared.
       area = 200_000
       countries = Country
         .where("surfacearea > ?", area)
         .where(continent: 'Europe')
+      # This test passes!
       expect(countries.count).to eq(13)
     end
 
     it "can find records via equality comparrison" do
-      #List the countries in Europe that have a life expectancy of more than 78?
+      # List the countries in Europe that have a life expectancy of more than 78?
+      countries = Country
       expect(countries.count).to eq(15)
     end
 
     it "can find records via equality comparrison" do
-      #List the countries in Europe that have a life expectancy of less than 77?
+      # List the countries in Europe that have a life expectancy of less than 77?
       expect(countries.count).to eq(22)
     end
 
     it "can combine comaparisons" do
-      #List the countries in Europe that have a life expectancy of less than 77 and surfacearea less than 50,000 km.?
+      # List the countries in Europe that have a life expectancy of less than 77 and surfacearea less than 50,000km?
       expect(countries.count).to eq(7)
     end
 
     it "can find records via equality comparrison" do
-      #List the countries that have a population smaller than 30,000,000 and a life expectancy of more than 45?
+      # List the countries that have a population larger than 30,000,000 and a life expectancy of more than 45?
       expect(countries.count).to eq(35)
     end
 
     it "can find records via multiple equality comparrisons" do
-      #List the countries in Africa that have a population smaller than 30,000,000 and a life expectancy of more than 45?
-      expect(countries.count).to eq(8)
+      # List the countries in Africa that have a population smaller than 30,000,000 and a life expectancy of more than 45?
+      countries = Country.where(continent: 'Africa').where('popultion < 30000000').where('lifeexpectancy > 45').count
+      expect(countries.count).to eq(37)
    end
 
     it "can find records using wildcards" do
-      #Which countries are something like a republic?
-      #(are there 122 or 143 countries or ?)
+      # Which countries are something like a republic?
+      # (are there 122 or 143 countries or ?)
       expect(countries.count).to eq(143)
-
     end
 
     it "can have multiple selects" do
-      #Which countries are some kind of republic and achieved independence after 1945?
+      # Which countries are some kind of republic and achieved independence after 1945?
       expect(countries.count).to eq(92)
     end
   end
 
-  describe "Order" do
+
+  describe "practice with Active Record using ORDER syntax" do
     it "can use order (class)" do
       # Which country has the shortest, non-null, life expectancy?
       country = Country
@@ -81,6 +84,7 @@ RSpec.describe "Practice with ActiveRecord", type: :model do
         .order(:lifeexpectancy)
         .limit(1)
         .first
+      # This test passes!
       expect(country.code).to eq('ZMB')
     end
 
@@ -90,67 +94,57 @@ RSpec.describe "Practice with ActiveRecord", type: :model do
     end
 
     it "can use order" do
-      #Which is the smallest country by area
+      # Which is the smallest country by surfacearea?
       expect(country.code).to eq('VAT')
-
     end
 
     it "can use order" do
-      #which is the biggest country by area
+      # Which is the biggest country by surfacearea?
       expect(country.code).to eq('RUS')
-
-
     end
 
     it "can use order" do
-      #Which is the smallest country by population
+      # Which is the smallest country by population?
       expect(country.code).to eq('ATA')
     end
 
     it "can use order" do
-      #which is the biggest country by population
+      # Which is the biggest country by population?
       expect(country.code).to eq('CHN')
-
     end
 
     it "can combine order and limit (class)" do
       # Which 10 countries have the lowest life expectancy?
-      # Hint: use pluck
-
+      # Hint: Use .pluck
       countries = Country
       .order(:lifeexpectancy)
       .limit(10)
       .pluck(:name)
-
       expected = ["Zambia", "Mozambique", "Malawi", "Zimbabwe", "Angola", "Botswana", "Rwanda", "Swaziland", "Niger", "Namibia"]
-
+      # This test passes!
       expected.map do |country|
         expect(countries).to include(country)
       end
     end
 
     it "can combine order and limit" do
-      #Which five countries have the lowest population density?
+      # Which five countries have the lowest population density?
       expected = ["South Georgia and the South Sandwich Islands", "Bouvet Island", "Antarctica", "British Indian Ocean Territory", "Heard Island and McDonald Islands"]
-
       expected.map do |country|
         expect(country_names).to include(country)
       end
-
     end
 
     it "can combine order and limit" do
-      #which five countries have the highest population density?
+      # Which five countries have the highest population density?
       expected = ["Macao", "Monaco", "Hong Kong", "Singapore", "Gibraltar"]
-
       expected.map do |country|
         expect(country_names).to include(country)
       end
-
     end
 
     it "can combine order and limit" do
-      #Which are the 10 smallest countries by area?
+      # Which are the 10 smallest countries by area?
       expected = ["Holy See (Vatican City State)", "Monaco", "Gibraltar", "Tokelau", "Cocos (Keeling) Islands", "United States Minor Outlying Islands", "Macao", "Nauru", "Tuvalu", "Norfolk Island"]
       expected.map do |country|
         expect(countries).to include(country)
@@ -158,36 +152,35 @@ RSpec.describe "Practice with ActiveRecord", type: :model do
     end
   end
 
-  describe "Combining ActiveRecord and Plain Old Ruby (POR) (class)" do
-    it "can simplify 'with' queries" do
-      #Of the smallest 10 countries by population, which has the biggest gnp?
 
+  describe "combining Active Record and Plain Old Ruby (POR) (class)" do
+    it "can simplify 'with' queries" do
+      # Of the smallest 10 countries by population, which has the biggest gnp?
       countries = Country
         .order(:population)
         .limit(10)
-      smallest_biggest = countries.min{|a,b| b.gnp <=> a.gnp}
-
+      smallest_biggest = countries.min{|a, b| b.gnp <=> a.gnp}
+      # This test passes!
       expect(smallest_biggest.name).to eq("Holy See (Vatican City State)")
     end
 
     it "can simplify 'with' queries" do
-      #Of the largest 10 countries by surfacearea, which has the smallest gnp?
+      # Of the largest 10 countries by surfacearea, which has the smallest gnp?
       expect(smallest_biggest.name).to eq("Antarctica")
     end
 
     it "can simplify 'with' queries" do
-      #Of the biggest 10 countries by population, which has the biggest gnp?
+      # Of the biggest 10 countries by population, which has the biggest gnp?
       expect(biggest_biggest.name).to eq("United States")
-
     end
 
     it "can simplify 'aggregate' operations (class)" do
-      #What is the sum of surface area of the 10 biggest countries in the world?
+      # What is the sum of surface area of the 10 biggest countries in the world?
       expect(sum_total).to eq(84183610.0)
     end
 
     it "can simplify 'aggregate' operations" do
-      #What is the sum of surface area of the 10 least populated countries in the world?
+      # What is the sum of surface area of the 10 least populated countries in the world?
       expect(sum_total).to eq(13132258.4)
     end
   end
